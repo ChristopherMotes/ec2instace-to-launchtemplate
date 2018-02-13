79aab859a87c8503642a251a0985e58a8821fb1f
Christopher Motes <chris.motes@acumera.net>
2018-02-13 15:48:06 -0600

    create arg

--- create-template.py
+++ create-template.py
@@ -2,4 +2,10 @@
 import boto3
 import json
 import argparse
+
+parser = argparse.ArgumentParser(description='Required Inputs to create template')
+parser.add_argument('--instance-id', nargs=1, required=True)
+args = parser.parse_args()
+
 ec2Client = boto3.client('ec2')
+
