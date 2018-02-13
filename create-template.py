1feb47d0531c9737c8804715393c8df01b5a5ce1
Christopher Motes <chris.motes@acumera.net>
2018-02-13 15:56:29 -0600

    ec2 describe data

--- create-template.py
+++ create-template.py
@@ -6,6 +6,11 @@ import argparse
 parser = argparse.ArgumentParser(description='Required Inputs to create template')
 parser.add_argument('--instance-id', nargs=1, required=True)
 args = parser.parse_args()
-
+instanceId = args.instance_id[0]
 ec2Client = boto3.client('ec2')
-
+ec2Dict = ec2Client.describe_instances(
+    InstanceIds=[
+        instanceId
+    ],
+)
+print ec2Dict
